72229ce3a3748d3fa0f168fd70d012f9cd78d4fb
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-29 13:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	 event.preventDefault();
+  let radiusValue = document.getElementById("radius");
+	let radius = parseFloat(radiusValue.value);
+	let volume = document.getElementById("volume");
+	if(isNaN(radius) || radius<0){
+volume.value = "NaN";
+	return;
+	}
+	let result = (4/3) *Math.PI * radius ** 3;
+	volume.value = result.toFixed(4);
 } 
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
